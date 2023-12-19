750328e096120fcd53e092a34ef02b9bab054506
shetiah <93013533+shetiah@users.noreply.github.com>
2023-12-19 13:36:27 +0200

    Hoassam (#1)
    * sad
    
    * hooaa

--- mosalah.cpp
+++ mosalah.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 int main ()
 {
-    int x=0;
-    int y=109999;
-
-    int awsa=1101;
-
-    int aws=110011110;
-   int awsa=11011111;
+    int y=2939393;
+    int z=010101;
     
-    int aws=1100220;
-    int z=0;
-    bool salah =false;
+    bool salah =true;
+
+    int x=0;
+    int aws=110011110;
+    int awsa=11011111;
 }
