c9ee9028ad2dd65e560eb1bfa91f128c94d1d7f1
shetiah <abdrahmanshetiah@gmail.com>
2023-12-19 04:09:08 +0200

    merged

--- mosalah.cpp
+++ mosalah.cpp
@@ -5,15 +5,12 @@ int main ()
     int x=0;
     int y=10;
 
-<<<<<<< HEAD
     int awsa=1101;
 
     int aws=110011110;
-=======
-    int awsa=11011111;
+   int awsa=11011111;
     
     int aws=1100220;
->>>>>>> cb9e31012d17e8cf9bb9865b5c6fc62fce33adcf
     int z=0;
     bool salah =false;
 }
